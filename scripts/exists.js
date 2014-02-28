f889d4e10752922a3f7bbd552d12d7957eaa5314
Alex Dunn <dunn.alex@gmail.com>
2014-02-27 21:30:35 -0800

    working APP
    slow as hell above about 30 things tho

--- scripts/exists.js
+++ scripts/exists.js
@@ -20,26 +20,37 @@
         }
 
         else if ( mode.value !== "" && number !== "" ) {
+            number = parseInt(number);
             // http://www.codingforums.com/javascript-programming/172746-iterate-through-alphabet-javascript.html#post843843
 
             for ( var i = 0; i < number; i++ ) {
                 symbolization += "∃" + makeVariable(i) + " ";
             }
-            symbolization += "( ";
             if ( mode.value === "at-least" || mode.value === "exactly" ) {
+                symbolization += "( ";
                 // https://medium.com/html5-css3/7c80a4b731f8
                 // it's actually essential that you do `k==number` NOT `k===number`!
                 for ( var j=0,k=0; j < number && k < number; k++,j=(k==number)?j+1:j,k=(k==number)?k=0:k ) {
                     // prevents a≠a and prevents b≠a after a≠b
                     if ( j < k ) {
-                        symbolization += makeVariable(j) + "≠" + makeVariable(k);
-                        symbolization += " ";
+                        symbolization += makeVariable(j) + "≠" + makeVariable(k) + " ∧ ";
                     }
                 }
+                // remove the trailing "∧ "
+                symbolization = symbolization.slice(0,symbolization.length - 2);
             }
 
-//            if ( mode.value === "exactly" ) {
-            symbolization += " )";
+            if ( mode.value === "exactly" || mode.value === "at-most" ) {
+                console.log(number);
+                var forAllVar = makeVariable(number);
+                symbolization += (mode.value === "exactly" ? " ∧" : "") + " ∀" + forAllVar + " ( ";
+                for ( var l = 0; l < number; l++ ) {
+                    symbolization += forAllVar + "=" + makeVariable(l) + " ∨ ";
+                }
+                symbolization = symbolization.slice(0,symbolization.length - 2);
+                symbolization += (mode.value === "exactly" ? ")" : "");
+            }
+            symbolization += ")";
         }
         // only mess with the DOM like this once or else it's slow as hell
         result.innerHTML = symbolization;
