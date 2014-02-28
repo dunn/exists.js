5d8bec6a7e55365d64b734901c2a7de130c98c4a
Alex Dunn <dunn.alex@gmail.com>
2014-02-27 20:40:06 -0800

    good lord that is faster

--- scripts/exists.js
+++ scripts/exists.js
@@ -1,5 +1,6 @@
 (function() {
-"use strict";
+    "use strict";
+
     var mode = document.getElementById("mode");
     var things = document.getElementById("things");
     var result = document.getElementById("result");
@@ -9,6 +10,7 @@
     addListener(things, "keyup", symbolize);
 
     function symbolize () {
+        var symbolization;
         var number = things.value;
 
         if ( !parseInt(number) ) {
@@ -18,24 +20,23 @@
         else if ( mode.value !== "" && number !== "" ) {
             // http://www.codingforums.com/javascript-programming/172746-iterate-through-alphabet-javascript.html#post843843
             var vars = "abcdefghijklmnopqrstuvwxyz".split("");
-//            number -= 1;
-            result.innerHTML = "";
+
             for ( var i = 0; i < number; i++ ) {
-                result.innerHTML += "∃" + ( i < 26 ? vars[i] : vars[i % 26] + "<sub>" + Math.floor(i / 26) + "</sub>") + " ";
+                symbolization += "∃" + ( i < 26 ? vars[i] : vars[i % 26] + "<sub>" + Math.floor(i / 26) + "</sub>") + " ";
             }
-            result.innerHTML += "( ";
+            symbolization += "( ";
             // https://medium.com/html5-css3/7c80a4b731f8
             // it's actually essential that you do `k==number` NOT `k===number`!
             for ( var j=0,k=0; j < number && k < number; k++,j=(k==number)?j+1:j,k=(k==number)?k=0:k ) {
                 // prevents a≠a and prevents b≠a after a≠b
                 if ( j < k ) {
-                    result.innerHTML += ( j < 26 ? vars[j] : vars[j % 26] + "<sub>" + Math.floor(j / 26) + "</sub>") + "≠" + ( k < 26 ? vars[k] : vars[k % 26] + "<sub>" + Math.floor(k / 26) + "</sub>");
-                    result.innerHTML += " ";
-                    console.log(j + "," + k);
-               }
+                    symbolization += ( j < 26 ? vars[j] : vars[j % 26] + "<sub>" + Math.floor(j / 26) + "</sub>") + "≠" + ( k < 26 ? vars[k] : vars[k % 26] + "<sub>" + Math.floor(k / 26) + "</sub>");
+                    symbolization += " ";
+                }
             }
-            result.innerHTML += " )";
+            symbolization += " )";
         }
+        result.innerHTML = symbolization;
     }
 
     /// add event listener:
