f225b05340c114522c6b469c6f14e949f9cf8b36
Alex Dunn <dunn.alex@gmail.com>
2014-02-27 22:38:33 -0800

    css

--- scripts/exists.js
+++ scripts/exists.js
@@ -24,10 +24,10 @@
             // http://www.codingforums.com/javascript-programming/172746-iterate-through-alphabet-javascript.html#post843843
 
             for ( var i = 0; i < number; i++ ) {
-                symbolization += "∃" + makeVariable(i) + " ";
+                symbolization += "<span class=\"logic exists\">∃</span>" + makeVariable(i) + " ";
             }
             if ( mode.value === "at-least" || mode.value === "exactly" ) {
-                symbolization += "( ";
+                symbolization += "<span class=\"parens\">(</span> ";
                 // https://medium.com/html5-css3/7c80a4b731f8
                 // it's actually essential that you do `k==number` NOT `k===number`!
                 for ( var j=0,k=0; j < number && k < number; k++,j=(k==number)?j+1:j,k=(k==number)?k=0:k ) {
@@ -37,20 +37,19 @@
                     }
                 }
                 // remove the trailing "∧ "
-                symbolization = symbolization.slice(0,symbolization.length - 2);
+                symbolization = (number > 1 ? symbolization.slice(0,symbolization.length - 2) : symbolization);
             }
 
             if ( mode.value === "exactly" || mode.value === "at-most" ) {
-                console.log(number);
                 var forAllVar = makeVariable(number);
-                symbolization += (mode.value === "exactly" ? " ∧" : "") + " ∀" + forAllVar + " ( ";
+                symbolization += (mode.value === "exactly" && number > 1 ? " ∧" : "") + " <span class=\"logic forall\">∀</span>" + forAllVar + " <span class=\"parens\">(</span> ";
                 for ( var l = 0; l < number; l++ ) {
                     symbolization += forAllVar + "=" + makeVariable(l) + " ∨ ";
                 }
                 symbolization = symbolization.slice(0,symbolization.length - 2);
-                symbolization += (mode.value === "exactly" ? ")" : "");
+                symbolization += (mode.value === "exactly" ? "<span class=\"parens\">)</span>" : "");
             }
-            symbolization += ")";
+            symbolization += ( !(mode.value === "at-least" && number < 2) ? "<span class=\"parens\">)</span>" : "");
         }
         // only mess with the DOM like this once or else it's slow as hell
         result.innerHTML = symbolization;
