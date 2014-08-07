c81b1bedfd5f491a58fcb3ec3917f3bc12038857
Alex Dunn <dunn.alex@gmail.com>
2014-08-06 22:37:38 -0700

    line breaks everywhere

--- scripts/exists.js
+++ scripts/exists.js
@@ -12,9 +12,13 @@
     function symbolize () {
         function makeVariable(n) {
             // this might be marginally faster than .split("")?
-            // from http://www.codingforums.com/javascript-programming/172746-iterate-through-alphabet-javascript.html#post843843
+            // from
+            // http://www.codingforums.com/javascript-programming/172746-iterate-through-alphabet-javascript.html#post843843
             var vars = "abcdefghijklmnopqrstuvwxyz";
-            return n < 26 ? vars.charAt(n) : vars.charAt(n % 26) + "<sub>" + Math.floor(n / 26) + "</sub>";
+            return n < 26 ?
+                vars.charAt(n) :
+                vars.charAt(n % 26) + "<sub>" +
+                Math.floor(n / 26) + "</sub>";
         }
 
         // build the string in a variable so we don't have to keep
@@ -27,39 +31,66 @@
         if ( !number ) {
             symbolization += "CHOOSE AN INTEGER, FUCKER";
         }
-        else if ( mode.value !== "" && number !== "" ) {
-            // as long as there's a number, then go ahead and generate existential quantifiers binding that many variables
+        else if ( mode.value !== "" &&
+                  number !== "" ) {
+            // as long as there's a number, then go ahead and generate
+            // existential quantifiers binding that many variables
             for ( var i = 0; i < number; i++ ) {
-                symbolization += "<span class=\"logic exists\">∃</span>" + makeVariable(i) + " ";
+                symbolization +=
+                "<span class=\"logic exists\">∃</span>" +
+                    makeVariable(i) + " ";
             }
             // now if there's a minimum number of things...
-            if ( mode.value === "at-least" || mode.value === "exactly" ) {
-				// if symbolizing "at least one thing", there's going to be no closing paren, so no opening one either
-				if ( !(mode.value === "at-least" && number === 1)) {
-					symbolization += "<span class=\"parens\">(</span> ";
-				}
+            if ( mode.value === "at-least" ||
+                 mode.value === "exactly" ) {
+				             // if symbolizing "at least one thing", there's going to be no
+				             // closing paren, so no opening one either
+                if ( !(mode.value === "at-least" &&
+                       number < 2)) {
+                    symbolization +=
+                    "<span class=\"parens\">(</span> ";
+                }
                 // https://medium.com/html5-css3/7c80a4b731f8
-                // it's actually essential that you do `k==number` NOT `k===number`!
-                for ( var j=0,k=0; j < number && k < number; k++,j=(k==number)?j+1:j,k=(k==number)?k=0:k ) {
+                // it's actually essential that you do 'k==number' NOT
+                // 'k===number'!
+                for ( var j=0,k=0;
+                      j < number && k < number;
+                      k++,j=(k==number)?j+1:j,k=(k==number)?k=0:k ) {
                     // prevents a≠a and prevents b≠a after a≠b
                     if ( j < k ) {
-                        symbolization += makeVariable(j) + "≠" + makeVariable(k) + " ∧ ";
+                        symbolization +=
+                        makeVariable(j) + "≠" +
+                            makeVariable(k) + " ∧ ";
                     }
                 }
                 // remove the trailing "∧ "
-                symbolization = (number > 1 ? symbolization.slice(0,symbolization.length - 2) : symbolization);
+                symbolization = (number > 1 ?
+                                 symbolization.slice(0,symbolization.length - 2) :
+                                 symbolization);
             }
             // if there's a maximum number of things...
-            if ( mode.value === "exactly" || mode.value === "at-most" ) {
+            if ( mode.value === "exactly" ||
+                 mode.value === "at-most" ) {
                 var forAllVar = makeVariable(number);
-                symbolization += (mode.value === "exactly" && number > 1 ? " ∧" : "") + " <span class=\"logic forall\">∀</span>" + forAllVar + " <span class=\"parens\">(</span> ";
+                symbolization +=
+                     (mode.value === "exactly" && number > 1 ?
+                      " ∧" : "") +
+                         " <span class=\"logic forall\">∀</span>" +
+                         forAllVar +
+                         " <span class=\"parens\">(</span> ";
+
                 for ( var l = 0; l < number; l++ ) {
-                    symbolization += forAllVar + "=" + makeVariable(l) + " ∨ ";
+                    symbolization += forAllVar +
+                        "=" + makeVariable(l) + " ∨ ";
                 }
-                symbolization = symbolization.slice(0,symbolization.length - 2);
-                symbolization += (mode.value === "exactly" ? "<span class=\"parens\">)</span>" : "");
+                symbolization =
+                         symbolization.slice(0,symbolization.length - 2);
+                symbolization += (mode.value === "exactly" ?
+                                  "<span class=\"parens\">)</span>" : "");
             }
-            symbolization += ( !(mode.value === "at-least" && number < 2) ? "<span class=\"parens\">)</span>" : "");
+            symbolization +=
+                      ( !(mode.value === "at-least" && number < 2) ?
+                        "<span class=\"parens\">)</span>" : "");
         }
         // only mess with the DOM like this once or else it's slow as hell
         result.innerHTML = symbolization;
